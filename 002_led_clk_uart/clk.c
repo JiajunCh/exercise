03b511d860e3c2503ed3cd22e4adccc947a7388e
jiajun.chen <jiajun.ch@qq.com>
2018-03-29 00:35:05 +0800

    creat 002_led_clk_uart(dir) edit led.c clk.c

---
+++ 002_led_clk_uart/clk.c
@@ -0,0 +1,9 @@
+#include "clk.h"
+#include "s3c2440_soc.h"
+
+void clk_config(void){
+	LOCKTIME = 0xffffffff;
+	MPLLCON = (0x5c<<12 | )
+	CLKCON = 
+}
+
---
+++ 002_led_clk_uart/led.h
@@ -0,0 +1,19 @@
+#ifndef __LED_H
+#define __LED_H
+
+#define LED_ON		(0)
+#define LED_OFF		(1)
+#define LED_REV		(2)
+
+#define	LED1		(4)
+#define	LED2		(5)
+#define	LED4		(6)
+
+void led_config(void);
+void set_led(int led, int val)；
+
+
+
+#endif
+
+
